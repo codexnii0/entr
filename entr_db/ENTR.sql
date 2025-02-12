54efeb8a1a84a5d18207f929ec87d9bb62555ef3
codexnii0 <caynn03@gmail.com>
2025-02-13 01:28:52 +0800

    updates

--- entr_db/ENTR.sql
+++ entr_db/ENTR.sql
@@ -1,10 +1,11 @@
-/*
-create database entr_db;
+/*create database entr_db;*/
 use entr_db;
-*/
 
+/*
 create table users (
 	id int primary key auto_increment,
     name varchar(100) not null,
     email varchar(100) unique not null
     );
+*/
+
